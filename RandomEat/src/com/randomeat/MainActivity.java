8b30f497c184ed0a74f67ce13a9b690331502160
Bao <Bao@WWB>
2014-10-23 17:29:31 +0800

    2014年10月23日 17:29:25

--- RandomEat/src/com/randomeat/MainActivity.java
+++ RandomEat/src/com/randomeat/MainActivity.java
@@ -1,17 +1,27 @@
 package com.randomeat;
 
-import org.apache.cordova.DroidGap;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends DroidGap {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+public class MainActivity extends Activity {
 
-	super.loadUrl("file:///android_asset/www/index.html");
-	 	
-		
-	}
-
+    private final int SPLASH_DISPLAY_LENGHT = 3000; //—”≥Ÿ»˝√Î 
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable(){
+ 
+         @Override
+         public void run() {
+             Intent mainIntent = new Intent(MainActivity.this,StartProjectActivity.class);
+             MainActivity.this.startActivity(mainIntent);
+             MainActivity.this.finish();
+         }
+           
+        }, SPLASH_DISPLAY_LENGHT);
+    }
 }
---
+++ RandomEat/src/com/randomeat/StartProject.java
@@ -0,0 +1,5 @@
+package com.randomeat;
+
+public class StartProject {
+
+}
